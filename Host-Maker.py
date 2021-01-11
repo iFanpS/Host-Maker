d82085fbaf38ed2e4108383c48b3b17de7bd1e2a
iFanpS <69078912+iFanpS@users.noreply.github.com>
2021-01-11 10:21:22 +0000

    Add files via upload

---
+++ Host-Maker.py
@@ -0,0 +1,24 @@
+#Host-Maker
+#Author iFanpS
+
+import os, sys
+
+print("[ $ ] iFanpS Host-Maker")
+ip = input("[ ? ] Put ur GTPS IP:")
+os.system('cls')
+asu=False
+delIP=False
+try:
+	with open('host.txt', 'r') as konz:
+		if f"{ip} growtopia1.com" in konz.read():
+			print(f'[ = ] {ip} Already added before!')
+		else:
+			asu=True;
+		konz.close()
+	if asu:
+		with open('host.txt', 'a') as konz:
+			konz.write(f'{ip} growtopia1.com\n{ip} growtopia2.com')
+			print('[ + ] File is created')
+except:
+	print("[ ? ] Something error")
+input("[ ! ] Go away/Click anykey to close the door...")
