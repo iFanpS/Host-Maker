2c7c167767c4bb91dd60b3bf4207a3f50ef9fcd7
iFanpS <69078912+iFanpS@users.noreply.github.com>
2021-01-11 21:36:45 +0700

    Update Host-Maker.py

--- Host-Maker.py
+++ Host-Maker.py
@@ -6,13 +6,15 @@ print("[ $ ] iFanpS Host-Maker")
 ip = input("[ ? ] Put ur GTPS IP:")
 os.system('cls')
 asu=False
+kontol=True
 try:
-	with open('host.txt', 'a') as konz:
-		konz.write(f'{ip} growtopia1.com\n{ip} growtopia2.com')
+	if kontol:
+		with open('host.txt', 'a') as konz:
+			konz.write(f'{ip} growtopia1.com\n{ip} growtopia2.com')
 			print(f'[ = ] {ip} File is created')
-		else:
+	else:
 			asu=True;
-		konz.close()
+			konz.close()
 	if asu:
 		with open('host.txt', 'r') as konz:
 			if f'{ip} growtopia1.com' in file.read():
