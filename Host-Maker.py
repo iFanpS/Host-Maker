324413a9a279d8af9070e8f35225add2e051ddd8
iFanpS <69078912+iFanpS@users.noreply.github.com>
2021-01-11 20:49:40 +0700

    Update Host-Maker.py

--- Host-Maker.py
+++ Host-Maker.py
@@ -1,6 +1,5 @@
 #Host-Maker
 #Author iFanpS
-
 import os, sys
 
 print("[ $ ] iFanpS Host-Maker")
@@ -8,16 +7,16 @@ ip = input("[ ? ] Put ur GTPS IP:")
 os.system('cls')
 asu=False
 try:
-	with open('host.txt', 'r') as konz:
-		if f"{ip} growtopia1.com" in konz.read():
-			print(f'[ = ] {ip} Already added before!')
+	with open('host.txt', 'a') as konz:
+		konz.write(f'{ip} growtopia1.com\n{ip} growtopia2.com')
+			print(f'[ = ] {ip} File is created')
 		else:
 			asu=True;
 		konz.close()
 	if asu:
-		with open('host.txt', 'a') as konz:
-			konz.write(f'{ip} growtopia1.com\n{ip} growtopia2.com')
-			print('[ + ] File is created')
+		with open('host.txt', 'r') as konz:
+			if f'{ip} growtopia1.com' in file.read():
+				print(f'ip already in file')
 except:
 	print("[ ? ] Something error")
 input("[ ! ] Go away/Click anykey to close the door...")
